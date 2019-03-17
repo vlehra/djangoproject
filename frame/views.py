70726926e181846a9d552bc5b04200b497ba5340
vlehra <vaibhavlehra@gmail.com>
2019-03-17 15:58:54 +0530

    swastik v1.00

--- frame/views.py
+++ frame/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from products.forms import RawFeedbackForm
 from .models import Feedback
 
-
+@login_required
+		
 def home_view(request, *args, **kwargs):
 	ob = Feedback.objects.all()
 	if request.method == 'POST':
 		
 		form =RawFeedbackForm(request.POST)
+		
 		if form.is_valid():
 			form.save()
 			return redirect('home')
